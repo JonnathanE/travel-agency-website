1dd0a67a679011840223862e7e949dc309a7f241
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-12-11 02:21:10 -0500

    Add Indonesia TopSection Background Image

--- src/containers/TopSection/index.jsx
+++ src/containers/TopSection/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import tw from 'twin.macro';
 import { NavBar } from '../../components/NavBar';
+import BackgroundImage from '../../images/indonesia-high-res.jpg'
 
 const TopSectionContainer = styled.div`
     ${tw`
@@ -12,10 +13,27 @@ const TopSectionContainer = styled.div`
     `}
 `;
 
+const LandingSection = styled.div`
+    ${tw`
+        w-full
+        h-screen
+        flex
+        flex-col
+    `}
+
+    background-image: url(${BackgroundImage}),
+        linear-gradient(to left, #005b9c, #b1d1b148);
+    background-size: cover;
+    background-position: bootom 10% left;
+    background-blend-mode: overlay;
+`;
+
 export const TopSection = () => {
     return (
         <TopSectionContainer>
-            <NavBar />
+            <LandingSection>
+                <NavBar />
+            </LandingSection>
         </TopSectionContainer>
     )
 }
